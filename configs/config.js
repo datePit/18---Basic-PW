872080f4065bf8239bdb665f73b8fa33b779e311
Bu <m.bukatsich@knomary.by>
2023-06-28 00:11:46 +0300

    added 5 PW tests in auth.tests.spec

---
+++ configs/config.js
@@ -0,0 +1,24 @@
+// node ./configs/config.js     
+/////////////////////////////////////////////////////////////////////
+/////  Endpoints
+/////////////////////////////////////////////////////////////////////
+
+export let BASE_URL = 'https://www.automationexercise.com'
+export let login_URL = 'https://www.automationexercise.com/login'
+
+// (not yet) Already existed user
+export let testUserName = 'Auto_Guliver'
+export let testFirstName = 'Auto_Alfred'
+export let testLastName = 'Auto_Daniels'
+export let password = 'AAss22##'
+
+
+
+
+/* 
+const randomName = faker.person.fullName(); // Rowan Nikolaus
+const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+ */
+
+
+//console.log(BASE_URL)
